ed491de76e6f7926b551bf27f1d71fefac06d760
Aniket19c <aniketchaudhary19j@gmail.com>
2025-02-23 21:27:53 +0530

    Use case 2 Calculate Daily Employee Wage

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.Write("Enter the number of hours worked (max-8): ");
+            int workDuration = Convert.ToInt32(Console.ReadLine());
+            int totalWage;
+            int payRate = 30;
+
+            if (workDuration > 8)
+            {
+                Console.WriteLine("Invalid Input.");
+                totalWage = 0;
+            }
+            else if (workDuration >= 5 && workDuration <= 8)
+            {
+                Console.WriteLine("Great job! Your efforts are recognized.");
+                totalWage = workDuration * payRate;
+            }
+            else
+            {
+                Console.WriteLine("You need to contribute more work hours.");
+                totalWage = 0;
+            }
+
+            Console.WriteLine($"Your total earnings: Rs {totalWage}/-");
+        }
+    }
+}
