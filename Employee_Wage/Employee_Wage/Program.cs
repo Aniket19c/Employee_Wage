4d9ca1bb0ec0dd2c4d26ee6482e5318b5f736882
Aniket19c <aniketchaudhary19j@gmail.com>
2025-02-23 21:40:16 +0530

    Calculating Wages for a Month

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Enter daily working hours (1-8): ");
+            int dailyHours = Convert.ToInt32(Console.ReadLine());
+            int hourlyWage = 20;
+            int workDays = 20;
+
+            if (dailyHours < 1 || dailyHours > 8)
+            {
+                Console.WriteLine("Invalid input. Please enter hours between 1 to 8.");
+            }
+            else
+            {
+                int monthlySalary = dailyHours * hourlyWage * workDays;
+                Console.WriteLine($"Based on {dailyHours} hours per day, your total monthly wage is: Rs {monthlySalary}/-");
+            }
+        }
+    }
+}
