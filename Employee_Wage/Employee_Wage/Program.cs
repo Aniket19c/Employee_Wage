41aa7457aae89bc2456347276348f444f5d747a5
Aniket19c <aniketchaudhary19j@gmail.com>
2025-02-23 21:25:00 +0530

    Use case 1 check employee is Absent or Present

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Employee_Wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2); 
+
+            if (empCheck == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
